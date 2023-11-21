debac3380f34a041463e93b2228a2c600eb5399e
disanv03 <heddisanv@gmail.com>
2023-11-21 14:57:27 +0100

    improved #horizontal_move with ASCII values

--- lib/movement.rb
+++ lib/movement.rb
@@ -5,25 +5,17 @@ class Movement
 
   def horizontal_move(cell)
     result = []
-    col_chars = ('a'..'h').to_a
     piece = cell.content.downcase
     coord = cell.coordinate.chars
     rank = coord[1]
-    col_x = coord[0].ord - 'a'.ord
-    offset = nil
-
-    case piece
-    when 'r', 'q'
-      offset = 7
-    when 'k'
-      offset = 1
-    end
-
-    (1..offset).to_a.each do |i|
+    col_x = coord[0].ord
+    offset = piece == 'k' ? 1 : 7
+   
+    (1..offset).each do |i|
       right = col_x + i
       left = col_x - i
-      result << "#{col_chars[right]}#{rank}" if (0..7).include?(right)
-      result << "#{col_chars[left]}#{rank}" if (0..7).include?(left)
+      result << "#{right.chr}#{rank}" if ('a'.ord..'h'.ord).include?(right)
+      result << "#{left.chr}#{rank}" if ('a'.ord..'h'.ord).include?(left)
     end
     result.sort
   end
