996cab1f504e9566aa623f506cf435ce5e39e56b
disanv03 <gnuno_03@hotmail.com>
2024-11-04 03:45:49 +0100

    Modify PIECE hash to use Unicode & setting up display tests

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -6,10 +6,13 @@ describe Display do
   subject(:display) {described_class.new(board)}
 
   context 'shows a provided board' do
-    it 'displays a board' do
+    it 'displays starting board position' do
       board.make_board
       display.show_board
       expect {display.show_board}.to output.to_stdout
+    end
+
+    it 'display random position' do
     end
   end
 end
